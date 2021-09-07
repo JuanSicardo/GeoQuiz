353702e741c5b00163a5d9477e3445fbf021f8a5
Juan Antonio Sicardo Contreras <contacto@juansicardo.com>
2021-09-06 19:47:46 -0500

    Removed unnecessary logs and overridden lifecycle functions

--- app/src/main/java/com/bignerdranch/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/geoquiz/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -65,31 +64,6 @@ class MainActivity : AppCompatActivity() {
         toggleNavigationButtons()
         toggleAnswerButtons()
         updateQuestion()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
